950aa4b12ee77eddc9a2516bbc511be1d6337b08
ZINK Antoine <antoine.zink@mcc.ad.culture.fr>
2026-03-10 13:33:10 +0100

    change return value

--- R/grouped_z_project.R
+++ R/grouped_z_project.R
@@ -7,7 +7,7 @@
 #' @param grouped_size [numeric] (**with default**) number of image merged in a group
 #' @param FUN  [function]  (**with default**)  function between the merged images (default FUN=sum)
 #'
-#' @return tmp, n [list]
+#' @return im [array] limited to n slices with n=number of slice in the initial image divided by the grouped_size
 #'
 #' @export
 #'
@@ -18,11 +18,11 @@
 
 grouped_z_project<-
   function(im,grouped_size,FUN=sum){
-    nb_frames<-dim(im@data)[3]
+    nb_frames<-dim(im)[3]
     stopifnot(nb_frames%%grouped_size==0)
     tmp<-im
     n<-nb_frames/grouped_size
     for(i in 1:n)
-      tmp@data[,,i]<-apply(im@data[,,seq((i-1)*grouped_size+1,i*grouped_size)],c(1,2),FUN)
-    return(list(tmp=tmp,n=n))
+      tmp[,,i]<-apply(im[,,seq((i-1)*grouped_size+1,i*grouped_size)],c(1,2),FUN)
+    return(im=tmp[,,seq(1,n)])
   }
